a6282570199d9779110791ca52451aeebd1b61c7
Ignacio Gil <i.gil@versiondigital.es>
2016-10-13 14:17:09 +0200

    boola app 2

---
+++ boola/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=128, null=True, blank=True)
+    vat = models.CharField(max_length=32, null=True, blank=True)
+
+    def __str__(self):
+        return u"{}".format(self.name)
+
+
+class Content(models.Model):
+    title = models.CharField(max_length=1023)
+
+    def __str__(self):
+        return u"{}".format(self.title)
+
+
+class Booking(models.Model):
+    content = models.ForeignKey(Content)
+    client = models.ForeignKey(Client)
+    date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        __date = self.date.ctime() if self.date else ""
+        return u"{} - {} :: {}".format(__date,
+                                         self.content,
+                                         self.client)
