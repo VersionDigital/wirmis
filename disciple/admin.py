acc1e86809afe132ea0f6b41c7b30620e4902640
Ignacio Gil <i.gil@versiondigital.es>
2016-10-20 13:14:50 +0200

    Disks.capacity added

--- disciple/admin.py
+++ disciple/admin.py
@@ -1,6 +1,14 @@
+#from django import forms
 from django.contrib import admin
+#from django.contrib.admin.filters import SimpleListFilter
+
 from disciple.models import Disk, Dispatch
 
+class DiskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'content', 'in_transit',)
+    search_fields = ('content__title',)
+    list_filter = ('content__title', 'in_transit',)
 
-admin.site.register(Disk)
+
+admin.site.register(Disk, DiskAdmin)
 admin.site.register(Dispatch)
--- disciple/models.py
+++ disciple/models.py
@@ -7,6 +7,7 @@ class Disk(models.Model):
     name = models.CharField(max_length=8)
     in_transit = models.BooleanField(_(u"out of office?"), default=False)
     content = models.ForeignKey(Content, null=True)
+    capacity = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return "{}".format(self.name)
@@ -15,3 +16,6 @@ class Dispatch(models.Model):
     date = models.DateField()
     disk = models.ForeignKey(Disk)
     client = models.ForeignKey(Client)
+
+    def __str__(self):
+        return "{} - {} - {}".format(self.date, self.disk.name, self.client.name)
